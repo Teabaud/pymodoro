9790a73221f0f034cb783e34cd32f1087ea840f8
Thibaud Veron <thibaud.veron@free.fr>
2026-03-05 21:16:06 +0100

    Fix for QSoundEffect bug on computer sleep

--- src/pymodoro/notification_sound.py
+++ src/pymodoro/notification_sound.py
@@ -2,8 +2,6 @@ from __future__ import annotations
 
 from pathlib import Path
 
-from loguru import logger
-
 # isort: split
 from PySide6 import QtCore
 from PySide6.QtCore import QUrl
@@ -15,15 +13,9 @@ NOTIFICATION_SOUND_FILE_PATH = (
 
 
 class NotificationSoundPlayer(QtCore.QObject):
-    def __init__(self, parent: QtCore.QObject | None = None) -> None:
-        super().__init__(parent)
-        self._sound_effect: QSoundEffect | None = None
-
     def play(self) -> None:
-        if self._sound_effect is None:
-            effect = QSoundEffect(self)
-            effect.setSource(QUrl.fromLocalFile(NOTIFICATION_SOUND_FILE_PATH))
-            effect.setLoopCount(1)
-            effect.setVolume(0.8)
-            self._sound_effect = effect
-        self._sound_effect.play()
+        effect = QSoundEffect(self)
+        effect.setSource(QUrl.fromLocalFile(NOTIFICATION_SOUND_FILE_PATH))
+        effect.setLoopCount(1)
+        effect.setVolume(0.8)
+        effect.play()
